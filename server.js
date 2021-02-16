9af9d4117d190405e16f31dec376d349aef41267
ElsaiedSamaka <elsaied.samaka1997@gmail.com>
2021-02-16 17:40:54 +0200

    final commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import request from "request";
+const app = express();
+
+const apiKey = "e5dc8edab856e1ade47aab982752339a";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("index", { weather: null, error: null });
+});
+
+app.post("/", function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render("index", { weather: null, error: "Error, please try again" });
+    } else {
+      let weather = JSON.parse(body);
+      if (weather.main == undefined) {
+        res.render("index", {
+          weather: null,
+          error: "Error, please try again",
+        });
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render("index", { weather: weatherText, error: null });
+      }
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
